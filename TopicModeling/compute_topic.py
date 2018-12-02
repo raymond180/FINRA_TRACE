d87729519beee94d0601bdc89bfec831246d297c
raymond180 <raymond180@hotmail.com>
2018-12-02 02:05:05 -0500

    lda model

---
+++ TopicModeling/compute_topic.py
@@ -0,0 +1,59 @@
+# Import Library
+import pandas as pd
+import numpy as np
+import os
+from pathlib import Path
+from sklearn import preprocessing
+import warnings
+warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
+import gensim
+from gensim.test.utils import datapath
+# Import local files
+import get_data
+
+def data_groupby(data):
+    data_gb = data.groupby(by=['document','BOND_SYM_ID'])
+    return data_gb
+
+def get_matrix1():
+    data = get_data.get_data()
+    data_gb = data_groupby(data)
+    print("computing matrix_1 ......")
+    matrix_1 = data_gb['BOND_SYM_ID'].size().unstack(fill_value=0)
+    matrix_1 = matrix_1.sort_index(axis=1)
+    print("computing matrix_1 done!")
+    return matrix_1
+
+def get_id2word(matrix):
+    le = preprocessing.LabelEncoder()
+    le.fit(matrix.columns)
+    transform = le.transform(matrix.columns)
+    inverse_transform = le.inverse_transform(transform)
+    id2word = dict(zip(transform, inverse_transform))
+    return id2word
+
+def compute_topic(matrix,matrix_name,num_topics,workers=3,chunksize=10000,passes=20,iterations=50):
+    matrix_corpus = gensim.matutils.Dense2Corpus(matrix.values,documents_columns=False)
+    id2word = get_id2word(matrix)
+    print("LdaMulticore Start!!")
+    lda = gensim.models.ldamulticore.LdaMulticore(corpus=matrix_corpus,id2word=id2word,workers=workers, num_topics=num_topics, chunksize=chunksize, passes=passes,iterations=iterations)
+    print("LdaMulticore Done!!")
+    
+    model_name = "{}_{}topics".format(matrix_name,num_topics)
+    print("Saving Model as "+model_name)
+    
+    current_path = os.getcwd()
+    current_path = Path(current_path)
+    save_path = current_path.parent / ("./Data/LDAModel/{}/".format(model_name))
+    
+    try:
+        os.mkdir(save_path)
+    except OSError:  
+        print ("Creation of the directory %s failed" % save_path)
+    else:  
+        print ("Successfully created the directory %s " % save_path)
+    save_path = save_path / model_name
+    save_path = datapath(str(save_path))
+
+    lda.save(save_path)
+    print("Model successfully save at" + save_path)
---
+++ TopicModeling/main.py
@@ -0,0 +1,4 @@
+import compute_topic
+
+matrix_1 = compute_topic.get_matrix1()
+compute_topic.compute_topic(matrix_1,"matrix_1_testing",100)
