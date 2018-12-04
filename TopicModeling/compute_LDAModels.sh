b8f726f520eccde389a1cd6826f2a2d92d5338d9
raymond180 <raymond180@hotmail.com>
2018-12-04 16:58:05 -0500

    Update compute_LDAModels.sh

--- TopicModeling/compute_LDAModels.sh
+++ TopicModeling/compute_LDAModels.sh
@@ -1,2 +1,12 @@
+#SBATCH --job-name=topicModeling
+#SBATCH --output main.out.%j
+#SBATCH --error main.out.%j
+#SBATCH --time=05:00:00
+#SBATCH --qos=dpart
+#SBATCH --nodes=1
+#SBATCH --ntasks=16
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem 16gb
+
 source ~/miniconda3/bin/activate
-srun python ~/FINRA_TRACE/TopicModeling/main.py
+srun -N 1 --mem=16gb python ~/FINRA_TRACE/TopicModeling/main.py
