213533630409b1a293b263171416ad969f163dc7
raymond180 <raymond180@hotmail.com>
2018-12-05 23:48:59 -0500

    Update compute_LDAModels_distributed.sh

--- TopicModeling/compute_LDAModels_distributed.sh
+++ TopicModeling/compute_LDAModels_distributed.sh
@@ -5,8 +5,8 @@
 #SBATCH --error main.out.%j
 #SBATCH --time=08:00:00
 #SBATCH --qos=dpart
-#SBATCH --nodes=8
-#SBATCH --ntasks=8
+#SBATCH --nodes=6
+#SBATCH --ntasks=10
 #SBATCH --ntasks-per-node=2
 #SBATCH --cpus-per-task=8
 #SBATCH --mem 7821mb
