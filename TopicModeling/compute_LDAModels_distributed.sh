0b03409e1cf4a98ea370c7b180c5fa55b0a0197c
raymond180 <raymond180@hotmail.com>
2018-12-06 09:13:46 -0500

    Update compute_LDAModels_distributed.sh

--- TopicModeling/compute_LDAModels_distributed.sh
+++ TopicModeling/compute_LDAModels_distributed.sh
@@ -5,8 +5,8 @@
 #SBATCH --error main.out.%j
 #SBATCH --time=08:00:00
 #SBATCH --qos=dpart
-#SBATCH --nodes=6
-#SBATCH --ntasks=6
+#SBATCH --nodes=10
+#SBATCH --ntasks=10
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=16
 #SBATCH --mem 23938mb
@@ -16,14 +16,14 @@ source ~/miniconda3/bin/activate
 export PYRO_SERIALIZERS_ACCEPTED=pickle
 export PYRO_SERIALIZER=pickle
 
-srun -N 1 --ntasks=1 python -m Pyro4.naming -n 0.0.0.0
+srun -N 1 --ntasks=1 python -m Pyro4.naming -n 0.0.0.0 &
 
-srun python -m gensim.models.lsi_worker
-srun python -m gensim.models.lsi_worker
-srun python -m gensim.models.lsi_worker
-srun python -m gensim.models.lsi_worker
+srun python -m gensim.models.lsi_worker &
+srun python -m gensim.models.lsi_worker &
+srun python -m gensim.models.lsi_worker &
+srun python -m gensim.models.lsi_worker &
 
-srun -N 1 --ntasks=1 python -m gensim.models.lsi_dispatcher
+srun -N 1 --ntasks=1 python -m gensim.models.lsi_dispatcher &
 
 srun python ~/FINRA_TRACE/TopicModeling/main_distributed.py
 
