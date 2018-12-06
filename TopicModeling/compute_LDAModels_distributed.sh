011a0e79fa655e754478ceb8b4c4c30bfc74134f
raymond180 <raymond180@hotmail.com>
2018-12-06 12:01:54 -0500

    Update compute_LDAModels_distributed.sh

--- TopicModeling/compute_LDAModels_distributed.sh
+++ TopicModeling/compute_LDAModels_distributed.sh
@@ -26,13 +26,13 @@ srun bash -c 'export PYRO_LOGFILE=pyro.log'
 srun bash -c 'export PYRO_LOGLEVEL=DEBUG'
 
 
-srun -N 1 --ntasks=1 bash -c 'python -m Pyro4.naming -n 0.0.0.0'
+srun -N 1 --ntasks=1 bash -c 'python -m Pyro4.naming -n 0.0.0.0' &
 
 srun python -m gensim.models.lsi_worker &
 srun python -m gensim.models.lsi_worker &
 srun python -m gensim.models.lsi_worker &
 srun python -m gensim.models.lsi_worker &
 
-srun -N 1 --ntasks=1 python -m gensim.models.lsi_dispatcher
+srun -N 1 --ntasks=1 python -m gensim.models.lsi_dispatcher &
 
 srun python ~/FINRA_TRACE/TopicModeling/main_distributed.py
