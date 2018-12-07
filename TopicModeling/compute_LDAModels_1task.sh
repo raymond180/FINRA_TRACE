b178123aefdaf46b9422f9b8ded9f9068d4ad500
raymond180 <raymond180@hotmail.com>
2018-12-06 20:38:12 -0500

    Create compute_LDAModels_1task.sh

---
+++ TopicModeling/compute_LDAModels_1task.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --job-name=topicModeling
+#SBATCH --output main.out.%j
+#SBATCH --error main.out.%j
+#SBATCH --time=08:00:00
+#SBATCH --qos=dpart
+#SBATCH --nodes=5
+#SBATCH --ntasks=1
+#SBATCH --mem 16gb
+
+source ~/miniconda3/bin/activate
+srun python ~/FINRA_TRACE/TopicModeling/main.py
