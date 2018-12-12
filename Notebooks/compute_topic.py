97031e0921d4ee3d7153edba593f196eda968dc4
raymond180 <raymond180@hotmail.com>
2018-12-12 15:19:52 -0500

    LDA analysis

--- Notebooks/compute_topic.py
+++ Notebooks/compute_topic.py
@@ -13,14 +13,15 @@ import logging
 
 # Import local files
 import get_data
-from manage_path import get_current_directory,create_directory
+from manage_path import *
 
 def data_groupby():
-    data = get_data.get_data()
+    data = get_data.load_data()
     data_gb = data.groupby(by=['document','BOND_SYM_ID'])
     return data_gb
 
 def compute_matrix1():
+    """Compute matrix_1 which is count of bonds on given dealer and day"""
     data_gb = data_groupby()
     print("computing matrix_1 ......")
     matrix_1 = data_gb['BOND_SYM_ID'].size().unstack(fill_value=0)
@@ -28,29 +29,41 @@ def compute_matrix1():
     print("computing matrix_1 done!")
     return matrix_1
 
+def compute_matrix2():
+    data_gb = data_groupby()
+    print("computing matrix_2 ......")
+    matrix_2 = data_gb['ENTRD_VOL_QT'].sum().unstack(fill_value=0)
+    matrix_2 = matrix_2.sort_index(axis=1)
+    print("computing matrix_2 done!")
+    return matrix_2
+
+def compute_matrix3():
+    data_gb = data_groupby()
+    print("computing matrix_3 ......")
+    data_gb['cap'] = pd.eval(data_gb['ENTRD_VOL_QT'] * data_gb['RPTF_PR'])
+    matrix_3 = data_gb['cap'].sum().unstack(fill_value=0)
+    matrix_3 = matrix_3.sort_index(axis=1)
+    print("computing matrix_3 done!")
+    return matrix_3
+
 def compute_corpus(matrix):
+    """Compute corpus given a matrix"""
     corpus = gensim.matutils.Dense2Corpus(matrix.values,documents_columns=False)
     return corpus
 
-def save_corpus(corpus,file_name):
-    current_path = os.getcwd()
-    current_path = Path(current_path)
-    corpus_save_path = current_path.parent / "./Data/Corpus/"
-    try:
-        os.mkdir(corpus_save_path)
-    except OSError:  
-        print ("Creation of the directory %s failed" % corpus_save_path)
-    else:  
-        print ("Successfully created the directory %s " % corpus_save_path)
-    file_name = corpus_save_path / "{}.mm".format(file_name)
+def save_corpus(corpus,corpus_save_name):
+    """Save the corpus given copus object and """
+    corpus_directory = get_corpus_directory
+    if not current_path.is_dir():
+        create_directory(corpus_directory)
+    file_name = corpus_directory / "{}.mm".format(corpus_save_name)
     gensim.corpora.MmCorpus.serialize(str(file_name), corpus)
     
 def load_corpus(file_name):
+    """Load the saved corpus"""
     print("loading corpus...")
-    current_path = os.getcwd()
-    current_path = Path(current_path)
-    corpus_load_path = current_path.parent / "./Data/Corpus/"
-    file_name = corpus_load_path / "{}.mm".format(file_name)
+    corpus_directory = get_corpus_directory()
+    file_name = corpus_directory / "{}.mm".format(file_name)
     file_name = str(file_name)
     corpus = gensim.corpora.MmCorpus(file_name)
     print("corpus successfully loaded!!")
@@ -63,12 +76,10 @@ def compute_id2word(matrix,matrix_name,save=True):
     transform = le.transform(matrix.columns)
     inverse_transform = le.inverse_transform(transform)
     id2word = dict(zip(transform, inverse_transform))
-    print("saving id2word ...")
     if(save):
-        current_path = get_current_directory()
-        id2word_save_path = current_path.parent / "./Data/id2word/"
-        create_directory(id2word_save_path)
-        file_name = id2word_save_path / "{}.npy".format(file_name)
+        print("saving id2word ...")
+        id2word_directory = get_id2word_directory()
+        file_name = id2word_directory / "{}.npy".format(file_name)
         # save the id2word using numpy
         np.save(file_name, id2word)
         print("id2word saved!!")
@@ -77,18 +88,18 @@ def compute_id2word(matrix,matrix_name,save=True):
 
 def load_id2word(id2word_name):
     print("loading id2word ...")
-    current_path = get_current_directory()
-    id2word_save_path = current_path.parent / "./Data/id2word/"
-    id2word_save_path = id2word_save_path / "{}.npy".format(id2word_name)
+    id2word_directory = get_id2word_directory()
+    id2word_save_path = id2word_directory / "{}.npy".format(id2word_name)
     # load the id2word using numpy
     id2word = np.load(id2word_save_path).item()
     print("id2word loaded!!")
     return id2word
 
-def compute_topic(corpus_name,corpus,num_topics,id2word,workers=3,chunksize=10000,passes=20,iterations=400):
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+def compute_topic(corpus_name,corpus,num_topics,id2word,workers=3,chunksize=10000,passes=40,iterations=400):
+    log_filename = "./logs/{}_{}topics.log".format(corpus_name,num_topics)
+    logging.basicConfig(filename=log_filename,format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     print("LdaMulticore Start!!")
-    lda = gensim.models.ldamulticore.LdaMulticore(corpus=corpus,id2word=id2word,workers=workers, num_topics=num_topics, chunksize=chunksize, passes=passes,iterations=iterations)
+    lda = gensim.models.ldamulticore.LdaMulticore(corpus=corpus,id2word=id2word,workers=workers, num_topics=num_topics, chunksize=chunksize, passes=passes,iterations=iterations,dtype=np.float64)
     print("LdaMulticore Done!!")
     
     model_name = "{}_{}topics".format(corpus_name,num_topics)
@@ -109,9 +120,10 @@ def compute_topic(corpus_name,corpus,num_topics,id2word,workers=3,chunksize=1000
     print("Model successfully save at" + save_path)
 	
 def compute_topic_distributed(corpus_name,corpus,num_topics,id2word,chunksize=10000,passes=40,iterations=400):
-    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    log_filename = "{}_{}topics.log".format(corpus_name,num_topics)
+    logging.basicConfig(filename=log_filename,format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     print("LdaMulticore Start!!")
-    lda = gensim.models.ldamodel.LdaModel(corpus=corpus,id2word=id2word, num_topics=num_topics, chunksize=chunksize, passes=passes,iterations=iterations,distributed=True)
+    lda = gensim.models.ldamodel.LdaModel(corpus=corpus,id2word=id2word, num_topics=num_topics, chunksize=chunksize, passes=passes,iterations=iterations,distributed=True,dtype=np.float64)
     print("LdaMulticore Done!!")
     
     model_name = "{}_{}topics".format(corpus_name,num_topics)
--- Notebooks/get_data.py
+++ Notebooks/get_data.py
@@ -4,11 +4,12 @@ import numpy as np
 import os
 from pathlib import Path
 
-def read_data():
-    root_folder = Path('../Data/')
-    file_name = 'TRACE2014_jinming.csv'
+from manage_path import *
 
-    file_path = root_folder / file_name
+def read_data(file_name):
+    dataset_directory = get_dataset_directory()
+    file_path = dataset_directory / file_name
+    # Only get the field we want
     field_of_interest_datetime = ['BOND_SYM_ID','CUSIP_ID','SCRTY_TYPE_CD','ENTRD_VOL_QT','RPTD_PR','RPT_SIDE_CD' \
                                   ,'TRD_EXCTN_DT_D','EXCTN_TM_D','TRD_RPT_DT','TRD_RPT_TM', 'Report_Dealer_Index'\
                                   ,'Contra_Party_Index','TRC_ST']
@@ -18,7 +19,6 @@ def read_data():
 
     parse_dates = {'TRD_RPT_DTTM':['TRD_RPT_DT','TRD_RPT_TM'],'TRD_EXCTN_DTTM':['TRD_EXCTN_DT_D','EXCTN_TM_D']}
 
-
     data = pd.read_csv(file_path,usecols=field_of_interest_datetime,parse_dates=parse_dates\
                        ,infer_datetime_format=True,converters={'TRD_RPT_TM':lambda x : pd.to_datetime(x,format='%H%M%S')})
     
@@ -27,18 +27,25 @@ def read_data():
     data['document'] = data.apply(lambda x: str(x['Report_Dealer_Index'])+ ',' +str(x['document_date']) ,axis=1)
     return data
 
-def to_pickle():
-    root_folder = Path('../Data/Pickle/')
-    file_name = 'TRACE2014_jinming'
-    file_path = root_folder / file_name
+def save_to_pickle(data,pickle_name):
+    """save data to pickle directory"""
+    # get pickle directory
+    pickle_directory = get_Pickle_directory()
+    # create directory if not exist
+    if not pickle_directory.is_dir():
+        create_directory(pickle_directory)
+    # concatonate pickle directory and pickle name
+    file_path = pickle_directory / pickle_name
+    # data to pickle
     data.to_pickle(file_path)
     
-def get_data():
-    root_folder = Path('../Data/Pickle/')
-    file_name = 'TRACE2014_jinming'
-    file_path = root_folder / file_name
+def load_data(file_name="TRACE2014_jinming"):
+    print("loading data {}...".format(file_name))
+    pickle_directory = get_Pickle_directory()
     
-    print("Getting data from{}...".format(file_path))
-    data = pd.read_pickle(file_path)
+    pickle_file_path = pickle_directory / file_name
+    
+    print("Getting data from{}...".format(pickle_file_path))
+    data = pd.read_pickle(pickle_file_path)
     print("Data getting success!")
     return data
