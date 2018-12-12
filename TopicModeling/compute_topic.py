37d3a0c930b7f35b02be4a4939edb5b4412003dc
Raymond Chen <raymond180@hotmail.com>
2018-12-11 19:03:25 -0500

    updated path management

--- TopicModeling/compute_topic.py
+++ TopicModeling/compute_topic.py
@@ -13,7 +13,7 @@ import logging
 
 # Import local files
 import get_data
-from manage_path import get_current_directory,create_directory
+from manage_path import *
 
 def data_groupby():
     data = get_data.load_data()
@@ -21,6 +21,7 @@ def data_groupby():
     return data_gb
 
 def compute_matrix1():
+    """Compute matrix_1 which is count of bonds on given dealer and day"""
     data_gb = data_groupby()
     print("computing matrix_1 ......")
     matrix_1 = data_gb['BOND_SYM_ID'].size().unstack(fill_value=0)
@@ -46,26 +47,23 @@ def compute_matrix3():
     return matrix_3
 
 def compute_corpus(matrix):
+    """Compute corpus given a matrix"""
     corpus = gensim.matutils.Dense2Corpus(matrix.values,documents_columns=False)
     return corpus
 
-def save_corpus(corpus,file_name):
-    current_path = get_current_directory()
-    corpus_save_path = current_path.parent / "./Data/Corpus/"
-    try:
-        os.mkdir(corpus_save_path)
-    except OSError:  
-        print ("Creation of the directory %s failed" % corpus_save_path)
-    else:  
-        print ("Successfully created the directory %s " % corpus_save_path)
-    file_name = corpus_save_path / "{}.mm".format(file_name)
+def save_corpus(corpus,corpus_save_name):
+    """Save the corpus given copus object and """
+    corpus_directory = get_corpus_directory
+    if not current_path.is_dir():
+        create_directory(corpus_directory)
+    file_name = corpus_directory / "{}.mm".format(corpus_save_name)
     gensim.corpora.MmCorpus.serialize(str(file_name), corpus)
     
 def load_corpus(file_name):
+    """Load the saved corpus"""
     print("loading corpus...")
-    current_path = get_current_directory()
-    corpus_load_path = current_path.parent / "./Data/Corpus/"
-    file_name = corpus_load_path / "{}.mm".format(file_name)
+    corpus_directory = get_corpus_directory()
+    file_name = corpus_directory / "{}.mm".format(file_name)
     file_name = str(file_name)
     corpus = gensim.corpora.MmCorpus(file_name)
     print("corpus successfully loaded!!")
@@ -78,12 +76,10 @@ def compute_id2word(matrix,matrix_name,save=True):
     transform = le.transform(matrix.columns)
     inverse_transform = le.inverse_transform(transform)
     id2word = dict(zip(transform, inverse_transform))
-    print("saving id2word ...")
     if(save):
-        current_path = get_current_directory()
-        id2word_save_path = current_path.parent / "./Data/id2word/"
-        create_directory(id2word_save_path)
-        file_name = id2word_save_path / "{}.npy".format(file_name)
+        print("saving id2word ...")
+        id2word_directory = get_id2word_directory()
+        file_name = id2word_directory / "{}.npy".format(file_name)
         # save the id2word using numpy
         np.save(file_name, id2word)
         print("id2word saved!!")
@@ -92,9 +88,8 @@ def compute_id2word(matrix,matrix_name,save=True):
 
 def load_id2word(id2word_name):
     print("loading id2word ...")
-    current_path = get_current_directory()
-    id2word_save_path = current_path.parent / "./Data/id2word/"
-    id2word_save_path = id2word_save_path / "{}.npy".format(id2word_name)
+    id2word_directory = get_id2word_directory()
+    id2word_save_path = id2word_directory / "{}.npy".format(id2word_name)
     # load the id2word using numpy
     id2word = np.load(id2word_save_path).item()
     print("id2word loaded!!")
