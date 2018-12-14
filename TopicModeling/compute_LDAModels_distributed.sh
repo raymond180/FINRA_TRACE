1f3f8a9c91eb7b36419acf6f0fe720232b9f87a1
raymond180 <raymond180@hotmail.com>
2018-12-14 00:52:52 -0500

    Update compute_LDAModels_distributed.sh

--- TopicModeling/compute_LDAModels_distributed.sh
+++ TopicModeling/compute_LDAModels_distributed.sh
@@ -22,11 +22,16 @@ export PYRO_SERIALIZER=pickle
 
 echo "working directory = "$SLURM_SUBMIT_DIR
 
-srun --nodes=1 --ntasks=1 --wait=0 bash -c  'python -m Pyro4.naming -n 0.0.0.0' &
+srun --nodes=1 --ntasks=1 --wait=0 bash -c  'export PYRO_SERIALIZERS_ACCEPTED=pickle;export PYRO_SERIALIZER=pickle;python -m Pyro4.naming -n 0.0.0.0' &
 
 python ~/FINRA_TRACE/TopicModeling/sleep.py
 
 srun --nodes=6 --ntasks=6 --wait=0 bash -c 'export PYRO_SERIALIZERS_ACCEPTED=pickle;export PYRO_SERIALIZER=pickle;python -m gensim.models.lda_worker' &
+srun --nodes=6 --ntasks=6 --wait=0 bash -c 'export PYRO_SERIALIZERS_ACCEPTED=pickle;export PYRO_SERIALIZER=pickle;python -m gensim.models.lda_worker' &
+srun --nodes=6 --ntasks=6 --wait=0 bash -c 'export PYRO_SERIALIZERS_ACCEPTED=pickle;export PYRO_SERIALIZER=pickle;python -m gensim.models.lda_worker' &
+srun --nodes=6 --ntasks=6 --wait=0 bash -c 'export PYRO_SERIALIZERS_ACCEPTED=pickle;export PYRO_SERIALIZER=pickle;python -m gensim.models.lda_worker' &
+
+python ~/FINRA_TRACE/TopicModeling/sleep.py
 
 srun --nodes=1 --ntasks=1 --wait=0 bash -c 'export PYRO_SERIALIZERS_ACCEPTED=pickle;export PYRO_SERIALIZER=pickle;python -m gensim.models.lda_dispatcher' &
 
