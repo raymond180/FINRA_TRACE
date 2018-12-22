f4953594dd34962eafb4ae00b5f077d4181481c0
Raymond Chen <raymond180@hotmail.com>
2018-12-21 21:10:30 -0500

    Update main_argument.py

--- TopicModeling/main_argument.py
+++ TopicModeling/main_argument.py
@@ -2,10 +2,10 @@ from compute_topic import load_corpus,load_id2word,compute_topic
 import sys
 
 def main():
-    corpus = str(sys.argv[0])
-    id2word = str(sys.argv[1])
-    corpus_name = str(sys.argv[2])
-    num_topics = int(sys.argv[3])
+    corpus = str(sys.argv[1])
+    id2word = str(sys.argv[2])
+    corpus_name = str(sys.argv[3])
+    num_topics = int(sys.argv[4])
 	
     corpus = load_corpus(corpus)
     id2word = load_id2word(id2word)
