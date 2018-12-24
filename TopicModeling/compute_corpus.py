407a34c818c67ba0d5acc4b74c8ad4b13931b5d6
Raymond Chen <raymond180@hotmail.com>
2018-12-24 16:24:29 -0500

    Update compute_corpus.py

--- TopicModeling/compute_corpus.py
+++ TopicModeling/compute_corpus.py
@@ -1,7 +1,10 @@
+from get_data import load_data
+from manage_path import *
 from compute_topic import *
 import sys
 
 def main():
+    data = load_data('TRACE2014_jinming.pkl')
     Dc_v3 = compute_Dc_v3(data)
     compute_corpus(Dc_v3,'Dc_v3')
     compute_id2word(Dc_v3,'Dc_v3')
