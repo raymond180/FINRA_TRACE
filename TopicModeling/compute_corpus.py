88a045f1bdfa3753b0fa83c190e6418ba0e1e56f
Raymond Chen <raymond180@hotmail.com>
2018-12-24 16:19:33 -0500

    Update compute_corpus.py

--- TopicModeling/compute_corpus.py
+++ TopicModeling/compute_corpus.py
@@ -3,8 +3,8 @@ import sys
 
 def main():
     Dc_v3 = compute_Dc_v3(data)
-	compute_corpus(Dc_v3,'Dc_v3')
-	compute_id2word(Dc_v3,'Dc_v3')
+    compute_corpus(Dc_v3,'Dc_v3')
+    compute_id2word(Dc_v3,'Dc_v3')
 
 if __name__ == "__main__":
     main()
