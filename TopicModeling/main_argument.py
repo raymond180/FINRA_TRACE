fd79b6e3634b80ab8ac3fbbcb12f04a4f2d7db41
Raymond Chen <raymond180@hotmail.com>
2019-01-31 17:02:04 -0500

    clean up

--- TopicModeling/main_argument.py
+++ TopicModeling/main_argument.py
@@ -2,6 +2,7 @@ from compute_topic import load_corpus,load_id2word,compute_topic
 import sys
 
 def main():
+    "pass command line arguments and compute_topic, used for computing LDA parallel on cluster"
     corpus = str(sys.argv[1])
     id2word = str(sys.argv[2])
     corpus_name = str(sys.argv[3])
@@ -9,8 +10,8 @@ def main():
 	
     corpus = load_corpus(corpus)
     id2word = load_id2word(id2word)
-	
-    compute_topic(corpus_name=corpus_name,corpus=corpus,num_topics=num_topics,id2word=id2word,workers=11,chunksize=12500,passes=60,iterations=1000)
+    compute_topic(corpus_name=corpus_name,corpus=corpus,num_topics=num_topics \
+                  ,id2word=id2word,workers=11,chunksize=12500,passes=60,iterations=1000)
 
 if __name__ == "__main__":
     main()
