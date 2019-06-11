49f18e4d597f3b4ae64eac051224deb4255ce451
Raymond Chen <raymond180@hotmail.com>
2019-06-11 18:01:35 +0800

    handel ValueError: Unstacked DataFrame is too big, causing int32 overflow

--- TopicModeling/compute_lda.py
+++ TopicModeling/compute_lda.py
@@ -176,8 +176,8 @@ def trade_vol_BoW(data,cap="large"):
     data_gb_buy = data[data['trade_vol_BoW_B']!='nan'].groupby(by=['trade_vol_BoW_B','BOND_SYM_ID'])
     
     print("computing bag_of_words ......")
-    bag_of_words = data_gb_sell['price'].sum().unstack(fill_value=0)
-    bag_of_words = bag_of_words.append(data_gb_buy['price'].sum().unstack(fill_value=0))
+    bag_of_words = data_gb_sell['price'].sum().unstack()
+    bag_of_words = bag_of_words.append(data_gb_buy['price'].sum().unstack())
     bag_of_words = bag_of_words.sort_index(axis=1)
     print("computing bag_of_words done!")
     return bag_of_words
@@ -258,6 +258,7 @@ def compute_matrix3():
 def compute_corpus(bag_of_words,corpus_save_name,save=True):
     """Compute corpus given a bag_of_words and save it"""
     print("computing corpus...")
+    bag_of_words = bag_of_words.fillna(0)
     corpus = gensim.matutils.Dense2Corpus(bag_of_words.values,documents_columns=False)
     print("corpus computed!!")
     if save:
