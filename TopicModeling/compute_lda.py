b9b8489d221ebf8b2cd3e9377abfee3c562578b6
Raymond Chen <raymond180@hotmail.com>
2019-06-11 18:41:59 +0800

    handel ValueError: Unstacked DataFrame is too big, causing int32 overflow

--- TopicModeling/compute_lda.py
+++ TopicModeling/compute_lda.py
@@ -176,8 +176,8 @@ def trade_vol_BoW(data,cap="large"):
     data_gb_buy = data[data['trade_vol_BoW_B']!='nan'].groupby(by=['trade_vol_BoW_B','BOND_SYM_ID'])
     
     print("computing bag_of_words ......")
-    bag_of_words = data_gb_sell['price'].sum().astype(np.float64).unstack(level=0)
-    bag_of_words = bag_of_words.append(data_gb_buy['price'].sum().astype(np.float64).unstack(level=-1))
+    bag_of_words = data_gb_sell['price'].sum().astype(np.float32).unstack(level=-1)
+    bag_of_words = bag_of_words.append(data_gb_buy['price'].sum().astype(np.float32).unstack(level=-1))
     bag_of_words = bag_of_words.sort_index(axis=1)
     print("computing bag_of_words done!")
     return bag_of_words
